1e46b47139977f4f14cda53cbff95900bf4fae25
Julian Berman <Julian@GrayVines.com>
2015-10-23 10:37:00 -0400

    Take the classifier path on the CLI.

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
+import sys
+
 from bp.filepath import FilePath
 
-from _opencv import ffi, lib
+from _opencv import lib
 from opencv_cffi.io import Camera
 from opencv_cffi.gui import Window
 from opencv_cffi.object_detection import HaarClassifier
@@ -8,19 +10,12 @@ from opencv_cffi.object_detection import HaarClassifier
 
 ESCAPE = ord("\x1b")
 
-
+classifier = HaarClassifier.from_path(FilePath(sys.argv[1]))
 camera = Camera(index=0)
 
 
 def escape_is_pressed(milliseconds=20):
     return lib.cvWaitKey(milliseconds) == ESCAPE
-
-
-
-filepath = FilePath(
-    "/Users/Julian/Desktop/haarcascades/haarcascade_frontalface_default.xml",
-)
-classifier = HaarClassifier.from_path(filepath)
 
 
 with Window(name="Example") as window:
