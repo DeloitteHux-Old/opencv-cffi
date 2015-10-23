8126f1f81e50dd35c4bd13130d7a76e13f311cde
Julian Berman <Julian@GrayVines.com>
2015-10-23 11:42:58 -0400

    Transformz

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import sys
 
 from bp.filepath import FilePath
 
-from _opencv import lib
+from _opencv import ffi, lib
 from opencv_cffi.imaging import Camera
 from opencv_cffi.gui import Window
 from opencv_cffi.object_detection import HaarClassifier
@@ -18,12 +18,25 @@ def escape_is_pressed(milliseconds=20):
     return lib.cvWaitKey(milliseconds) == ESCAPE
 
 
+def uglify(frame, facetangle):
+    lib.cvSetImageROI(frame, facetangle.right_half._cv_rect)
+    lib.cvNot(frame, frame)
+    lib.cvResetImageROI(frame)
+
+
+def prettify(frame, facetangle):
+    facetangle.draw_onto(frame)
+
+
+transform = uglify if sys.argv[2] == "uglify" else prettify
+
+
 with Window(name="Example") as window:
     for frame in camera.frames():
         if escape_is_pressed():
             break
 
         for rectangle in classifier.detect_objects(inside=frame):
-            rectangle.draw_onto(frame)
+            transform(frame=frame, facetangle=rectangle)
 
         window.show(frame)
