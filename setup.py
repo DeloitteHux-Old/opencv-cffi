60ec7852b0bb81ade8c4a844f862774eb8ef4ef7
Julian Berman <Julian@GrayVines.com>
2015-10-26 11:21:23 -0400

    Remove the lying classifier.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ classifiers = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: Implementation :: CPython",
     "Programming Language :: Python :: Implementation :: PyPy"
